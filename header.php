351a92d87030175f28c8fa42e4c0fbba7a6b18f6
Daniel Pullan <dandandrumsgm@gmail.com>
2016-10-11 18:38:12 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,46 @@
+<head>
+    <meta charset="utf-8">
+    <title>Krypton</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <link href="krypton.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-static" role=" navigation ">
+        <div class="container ">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header ">
+                <button type="button " class="navbar-toggle " data-toggle="collapse " data-target="#bs-example-navbar-collapse-1 ">
+                    <span class="sr-only ">Toggle navigation</span>
+                    <span class="icon-bar "></span>
+                    <span class="icon-bar "></span>
+                    <span class="icon-bar "></span>
+                </button>
+                <a class="navbar-brand " href="# ">Krypton</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1 ">
+                <ul class="nav navbar-nav ">
+                    <li>
+                        <a href="# ">Group</a>
+                    </li>
+                    <li>
+                        <a href="# ">Upcoming</a>
+                    </li>
+                    <li>
+                        <a href="# ">Admin Panel</a>
+                    </li>
+                </ul>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a>Hi Batman</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
