5a698152d914b3dee8c3dab610a3b46fef0fde53
Daniel Pullan <dandandrumsgm@gmail.com>
2016-10-11 22:12:40 +0100

    added comment to config.php

--- core/config/config.php
+++ core/config/config.php
@@ -3,6 +3,9 @@
 
 //Server configuration (Database connection!)
 
+// the only password you'll see here is for Ryan/Space's local dev server.
+// the public dev server has a different login + password
+
 	$server = 'localhost'; //The IP for the datebase server
 	$server_user = 'root'; //The username for the database server
 	$server_password = '1123'; //The password for the datebase server
