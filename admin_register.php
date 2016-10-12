43626f4365a93259da219fc139597f810cb365a3
SpaceDux <ryan.a.williams@outlook.com>
2016-10-12 01:00:48 +0100

    Creating Functions
    Had to upload via Git Website, client isn't working too well :/
    
    Minor tweaks, 2 new files for functions.

--- admin_register.php
+++ admin_register.php
@@ -1,6 +1,8 @@
 <?php 
 	include ('/header.php');
-	require_once ('/core/config/dbcon.php');
+	require_once ('/core/init.php');
+	require_once ('/core/functions/fnc_chkLogged.php');
+	require_once ('/core/functions/fnc_chkAdmin.php');
 	
 	$message = '';
 	
@@ -19,67 +21,67 @@
 	$stmt->bindParam(':rank', $_POST['reg_rank']);
 	
 	if( $stmt->execute() ):
-		$message = '<div class="alert alert-success" role="alert"><b>Success!</b>, User has been added!</div>';
+		$message = '<div class="alert alert-success" role="alert"><b>Success!</b> User has been added!</div>';
 	else:
-		$message = '<div class="alert alert-danger" role="alert"><b>Error!</b>, User has not been added!</div>';
+		$message = '<div class="alert alert-danger" role="alert"><b>Error!</b> User has not been added!</div>';
 	endif;
 		
 	endif;
 ?>
+<div class="container">
+	<?php echo $message ?>
+	<form action="admin_register.php" method="POST">
+		<div class="form-group">
+			<label>Username</label>
+			<input type="text" class="form-control" name="reg_username" maxlength="24" placeholder="Username">
+		</div>
+		<div class="form-group">
+			<label>Password</label>
+			<input type="password" class="form-control" name="reg_password" placeholder="Password">
+		</div>
 
-
-    <div class="container">
-        <?php echo $message ?>
-            <form action="admin_register.php" method="POST">
-                <div class="form-group">
-                    <label>Username</label>
-                    <input type="text" class="form-control" name="reg_username" maxlength="24" placeholder="Username">
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" class="form-control" name="reg_password" placeholder="Password">
-                </div>
-
-                <div class="form-group">
-                    <label>Email</label>
-                    <input type="email" class="form-control" name="reg_email" placeholder="Email Address">
-                </div>
-                <div class="form-group">
-                    <label>First Name</label>
-                    <input type="text" class="form-control" name="reg_first_name" placeholder="First Name">
-                </div>
-                <div class="form-group">
-                    <label>Surname</label>
-                    <input type="text" class="form-control" name="reg_last_name" placeholder="Surname">
-                </div>
-                <div class="form-check">
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="radio" name="reg_gender" value="1" checked> Male
-                    </label>
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="radio" name="reg_gender" value="2"> Female
-                    </label>
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="radio" name="reg_gender" value="3"> Apache Attack Helicopter
-                    </label>
-                </div>
-                <div class="form-check">
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="radio" name="reg_rank" value="1" checked> Student
-                    </label>
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="radio" name="reg_rank" value="2"> Teacher
-                    </label>
-                    <label class="form-check-label">
-                        <input class="form-check-input" type="radio" name="reg_rank" value="3"> Administrator
-                    </label>
-                </div>
-                <div class="form-group">
-                    <label>Date of Birth</label>
-                    <div class="form-group">
-                        <input class="form-control" type="date" value="" name="reg_dob">
-                    </div>
-                </div>
-                <input class="btn btn-success pull-right" type="submit" value="Register"></input>
-            </form>
-    </div>
+		<div class="form-group">
+			<label>Email</label>
+			<input type="email" class="form-control" name="reg_email" placeholder="Email Address">
+		</div>
+		<div class="form-group">
+			<label>First Name</label>
+			<input type="text" class="form-control" name="reg_first_name" placeholder="First Name">
+		</div>
+		<div class="form-group">
+			<label>Surname</label>
+			<input type="text" class="form-control" name="reg_last_name" placeholder="Surname">
+		</div>
+		<div class="form-check">
+		  <label class="form-check-label">
+			<input class="form-check-input" type="radio" name="reg_gender" value="1" checked>
+			Male
+		  </label>
+		  <label class="form-check-label">
+			<input class="form-check-input" type="radio" name="reg_gender" value="2">
+			Female
+		  </label>
+		</div>
+		<div class="form-group">
+		  <label>Date of Birth</label>
+		  <div class="form-group">
+			<input class="form-control" type="date" value="" name="reg_dob">
+		  </div>
+		</div>
+		<div class="form-check">
+		  <label class="form-check-label">
+			<input class="form-check-input" type="radio" name="reg_rank" value="1" checked>
+			Student
+		  </label>
+		  <label class="form-check-label">
+			<input class="form-check-input" type="radio" name="reg_rank" value="2">
+			Teacher
+		  </label>
+		  <label class="form-check-label">
+			<input class="form-check-input" type="radio" name="reg_rank" value="3">
+			Administrator
+		  </label>
+		</div>
+		<input class="btn btn-success pull-right" type="submit" value="Register"></input>
+	</form>
+</div>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php include 'header.php'; ?>
-    <?php include 'content.php'; ?>
-        <?php include 'footer.php'; ?>
+<?php
+	require '/core/init.php';
+	require_once ('/core/functions/fnc_chkLogged.php');
+	
+	//Page
+	include 'header.php';
+	include 'content.php';
+	include 'footer.php';
+?>
