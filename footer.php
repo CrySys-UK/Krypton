f53b5c2230cd6e66aee88f8428a087eec244fc19
Daniel Pullan <dandandrumsgm@gmail.com>
2016-10-12 20:01:31 +0100

    minor text fixes

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 <footer>
     <div class="row ">
         <div class="col-lg-12 ">
-            <p>Copyright © Krypton Team 2016</p>
+            <p>Copyright © CrySys 2016</p>
         </div>
     </div>
     <!-- /.row -->
