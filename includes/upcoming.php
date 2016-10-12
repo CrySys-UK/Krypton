946becc2d169b70b48e216d216f2ad17d7c3b2fd
Daniel Pullan <dandandrumsgm@gmail.com>
2016-10-12 21:21:43 +0100

    Revert "added calendar"
    This reverts commit 2b48c7817274bd9ef9c1002f735623427b38e2c4.

--- includes/upcoming.php
+++
@@ -1,3 +0,0 @@
-<div class="container">
-    <iframe src="https://calendar.google.com/calendar/embed?src=k3nafbeprkllv2q45bvpvhodig%40group.calendar.google.com&ctz=Europe/London" style="border: 0" width="800" height="600" frameborder="0" scrolling="no">
-    </iframe>
