da4c87b604f8401ec7c777ee1172a0599c979c89
SpaceDux <ryan.a.williams@outlook.com>
2016-10-15 18:42:19 +0100

    news server db

--- core/config/config.php
+++ core/config/config.php
@@ -3,11 +3,11 @@
 
 //Server configuration (Database connection!)
 
-	$server = 'localhost'; //The IP for the datebase server
-	$server_user = 'root'; //The username for the database server
-	$server_password = 'new_password'; //The password for the datebase server
+	$server = 'sql8.freemysqlhosting.net'; //The IP for the datebase server
+	$server_user = 'sql8140228'; //The username for the database server
+	$server_password = 'ZzQR8lpyxH'; //The password for the datebase server
 	
-	$server_selDB = 'krypton'; //Select the database you want to connect to
+	$server_selDB = 'sql8140228'; //Select the database you want to connect to
 	
 	//Site Configuration
 	$server_url = 'http://localhost/krypton'; //Server URL example.com (DOES NOT END WITH /)
