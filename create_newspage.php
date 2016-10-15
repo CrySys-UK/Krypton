be266c1ebe1d31b36fe2a8871058b79f8e97b6d8
Daniel Pullan <dandandrumsgm@gmail.com>
2016-10-15 13:59:32 +0100

    starting work on news submit page

---
+++ create_newspage.php
@@ -0,0 +1,9 @@
+<?php
+	require '/core/init.php';
+	require_once ('/core/functions/fnc_chkLogged.php');
+	
+	//Page
+	include '/includes/header.php';
+	include '/includes/create_news.php';
+	include '/includes/footer.php';
+?>
---
+++ includes/create_news.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <div class="row">
+        <form role="form" id="contact-form" class="contact-form">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="newstitle" autocomplete="off" id="newstitle" placeholder="Title">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <textarea name="content" data-provide="markdown" rows="10"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <button type="submit" class="btn main-btn pull-right">Submit</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
