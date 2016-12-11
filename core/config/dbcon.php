516b995426fdabfb8bf716e98ed306bc730b8ca5
Daniel Pullan <dandandrumsgm@gmail.com>
2016-12-11 20:11:48 +0000

    removing more unneeded cruft

--- core/config/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once(__DIR__.'/config.php');
-	
-	try {
-		$dbConn = new PDO("mysql:host=$server;dbname=$server_selDB;", $server_user, $server_password);
-	} catch(PDOException $e) {
-		die("Failed to Connect: " . $e->getMessage());
-	}
-?>
--- includes/upcoming.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <div class="row">
-        <iframe src="https://commingly.com/krypton/events/calendar/" frameborder="0" allowTransparency="true" width="700" height="500"></iframe>
-    </div>
-</div>
